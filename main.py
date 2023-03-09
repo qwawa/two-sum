aebdf33bd570be364787ef378ce936391684cd0a
skku <127391061+qwawa@users.noreply.github.com>
2023-03-09 20:52:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def twoSum(self, nums: List[int], target: int) -> List[int]:
+    ans = [0, 0]
     for i in range(0, len(nums)):
-        for j in range(0, len(nums)):
-            if i == j:
-                continue
+        for j in range(i + 1, len(nums)):
             if nums[i] + nums[j] == target:
-                return [i, j]
+                ans = [i, j]
+    return ans
